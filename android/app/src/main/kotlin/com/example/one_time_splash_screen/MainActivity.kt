16e7d408e8dc2498be2e191a6411f474c0334a39
Romit Katrodiya <romitkatrodiya2308@gmail.com>
2022-09-09 18:50:30 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/one_time_splash_screen/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.one_time_splash_screen
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/global.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class Global {
+  static var appColor = const Color(0xff3F3D56);
+}
